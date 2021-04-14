6269c3b9866b11d6e6b2de07de5ace67cd08e592
Clark <clark.eustaquio@gmail.com>
2021-04-15 02:45:08 +0800

    Update

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Destination ,List
+from .models import Destination, TaskList, TaskType
 
 admin.site.register(Destination)
-admin.site.register(List)
+admin.site.register(TaskList)
+admin.site.register(TaskType)
